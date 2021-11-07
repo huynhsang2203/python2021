af870b8f5f3dde143d6c51118795b4fb2b814ba0
huynhsang2203 <huynhsang220300@gmail.com>
2021-11-08 01:42:04 +0700

    Update

--- web_profile/web/urls.py
+++ web_profile/web/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .import views
+from .views import LoginClass
 
 urlpatterns = [
-    path('login/', views.page_login, name='page_login'),
+    path('login/', LoginClass.as_view(), name='page_login'),
     path('register/', views.register, name='register'),
     path('register-success/', views.register_success, name='register_success'),
     path('', views.page_home, name='page_home')
--- web_profile/web/views.py
+++ web_profile/web/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render, HttpResponse
+from django.views import View
 
 
 # Create your views here.
-def page_login(request):
-    return render (request, './web/login.html')
+
 def page_home(request):
     return render (request, './web/home.html')
 def register(request):
     return render (request, './web/register_page.html')
 def register_success(request):
     return render (request, './web/register_done.html')
+
+class LoginClass(View):
+    def get( self, request):
+        return render (request, './web/login_page.html')
+    def post( self, request):
+        return render (request, './web/login_done.html')
